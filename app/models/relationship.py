a1e70b3d56605856743b6ec71b0dac7584c902f4
David Periscal García <davidperiscal@gmail.com>
2021-12-08 11:17:16 +0100

    Add the modelation of a diagram

---
+++ app/models/relationship.py
@@ -0,0 +1,7 @@
+class Relationship:
+    def __init__(self,sourceEntity, targetEntity, relationName, type):
+        self.sourceEntity = sourceEntity
+        self.targetEntity = targetEntity
+        self.relationName = relationName
+        self.type = type
+        
---
+++ app/models/system.py
@@ -0,0 +1,4 @@
+class System:
+    def __init__(self,name, relationships):
+    	self.name = name
+    	self.relationships = relationships
