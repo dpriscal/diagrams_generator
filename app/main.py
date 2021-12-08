13c86a3604cfa04be8529f367d274d305c6cfc34
David Periscal García <davidperiscal@gmail.com>
2021-12-08 12:03:13 +0100

    Create first diagram

--- app/main.py
+++ app/main.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI
 
+from app.infrastructure.system_diagram import SystemDiagram
+from app.models.entity import Entity
+from app.models.relationship import Relationship
+from app.models.system import System
+
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
+
+    entity1 = Entity("Entity 1", [])
+    entity2 = Entity("Entity 2", [])
+    relationship = Relationship(entity1, entity2, "relationship_test")
+    relationships = [relationship]
+    system = System("hellow_system", relationships)
+    diagram = SystemDiagram(system)
+    diagram.printSystem()
     return {"message": "Hello World"}
--- app/models/relationship.py
+++ app/models/relationship.py
@@ -1,6 +1,5 @@
 class Relationship:
-    def __init__(self, sourceEntity, targetEntity, relationName, type):
+    def __init__(self, sourceEntity, targetEntity, relationName):
         self.sourceEntity = sourceEntity
         self.targetEntity = targetEntity
         self.relationName = relationName
-        self.type = type
